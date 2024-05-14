7b579aac88c51777b8d5699987ccbe4fcb783602
ARGAO <jgabrielaragao@gmail.com>
2024-05-13 22:54:48 -0300

    aula03

--- includes/banco.php
+++ includes/banco.php
@@ -1,7 +1,13 @@
 <?php 
-    $banco = new mysqli("localhot", "root", "", "db_games");
+    $banco = new mysqli("localhost", "root", "", "db_games");
     if($banco->connect_errno){
         echo "<p> Encontrei um erro $banco->errno --> $banco->connect_error</p>";
         die();
     }
-?>
+
+    $banco->query("set names 'utf8'");
+    $banco->query("set character_set_connection=utf8");
+    $banco->query("set character_set_client=utf8");
+    $banco->query("set character_set_results=utf8");
+
+
--- index.php
+++ index.php
@@ -4,33 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Título da Página</title>
-    <link rel="stylesheet" href="Styles/style.css">
+    <link rel="stylesheet" href="estilos/estilo.css">
+    <?php 
+        function thumb($arq){
+            $caminho = "fotos/$arq";
+            if(is_null($arq) || !file_exists($caminho)){
+                return "fotos/indisponivel.png";
+            }else{
+                return $caminho;
+            }
+        }
+    ?>
+
 </head>
 <body>
+    <?php 
+        require_once "includes/banco.php";
+        require_once "includes/funcoes.php";
+
+    ?>
     <main id="corpo">
         <h1>Escolha seu jogo</h1>
         <table class="listagem">
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
-            <tr>
-                <td>Foto</td>
-                <td>Nome</td>
-                <td>Adm</td>
-            </tr>
+            <?php 
+                $busca = $banco->query("select * from jogos order by nome");
+                if(!$busca){
+                    echo "<tr><td>Infelizmente não foi possível realizar a busca";
+                }else{
+                    if ($busca->num_rows == 0){
+                        echo "<tr><td>Nenhum registro encontrado";
+                    } else{
+                        while ($reg=$busca->fetch_object()) {
+                            $t = thumb($reg->capa);
+                            echo "<tr><td><img src='$t' class='mini' />";
+                            echo "<td><a href='detalhes.php'>$reg->nome</a>";
+                            echo "<td>Adm";
+                        }
+                    }  
+                }
+            ?>
         </table>
     </main>
+    <?php $banco->close();?>
 </body>
 </html>
