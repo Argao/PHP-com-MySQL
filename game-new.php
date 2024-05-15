635e6928d942e571d65d4dd53a69b8b0c0fb60d9
ARGAO <jgabrielaragao@gmail.com>
2024-05-15 14:14:14 -0300

    Cadastro de Jogo

--- game-new.php
+++ game-new.php
@@ -24,67 +24,34 @@
             if (!isset($_POST['nome'])) {
                 require "game-new-form.php";
             } else {
+                $nome = $_POST['nome'] ?? null;
+                $descricao = $_POST['descricao'] ?? null;
+                $genero = $_POST['genero'] ?? null;
+                $produtora = $_POST['produtora'] ?? null;
+                $nota = $_POST['nota'] ?? null;
+                $capa = $_FILES['capa']['name'] ?? null;
 
-                $target_dir = "fotos/";
-                $target_file = $target_dir . basename($_FILES["capa"]["name"]);
-                $uploadOk = 1;
-                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                $q = "INSERT INTO jogos (nome, descricao, genero, produtora, nota, capa) VALUES ('$nome','$descricao','$genero','$produtora','$nota','$capa')";
 
-                // Check if image file is a actual image or fake image
-
-                $check = getimagesize($_FILES["capa"]["tmp_name"]);
-                if ($check !== false) {
-                    $uploadOk = 1;
-                } else {
-                    echo msg_erro("O arquivo não é uma imagem válida.");
-                    $uploadOk = 0;
-                }
-                
-
-                // Check if file already exists
-                if (file_exists($target_file)) {
-                    echo msg_erro("O arquivo já existe.");
-                    $uploadOk = 0;
-                }
-
-                // Check file size
-                if ($_FILES["capa"]["size"] > 500000) {
-                    echo msg_erro("O arquivo é muito grande.");
-                    $uploadOk = 0;
-                }
-
-                // Allow certain file formats
-                if (
-                    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                    && $imageFileType != "gif"
-                ) {
-
-                    echo msg_erro("Apenas arquivos JPG, JPEG, PNG e GIF são permitidos.");
-                    $uploadOk = 0;
-                }
-
-                // Check if $uploadOk is set to 0 by an error
-                if ($uploadOk == 0) {
-                    echo msg_erro("O arquivo não foi enviado.");
-                    // if everything is ok, try to upload file
-                } else {
-                    
-                    if (move_uploaded_file($_FILES["capa"]["tmp_name"], $target_file)) {
-                        $nome = $_POST['nome'];
-                        $descricao = $_POST['descricao'];
-                        $genero = $_POST['genero'];
-                        $produtora = $_POST['produtora'];
-                        $nota = $_POST['nota'];
-                        $capa = basename($_FILES["capa"]["name"]);
-                        $sql = "INSERT INTO jogos (nome, descricao, genero, capa, produtora, nota ) VALUES ('$nome','$descricao','$genero','$capa','$produtora','$nota')";
-                        $banco->query($sql);
+                if ($capa == "" || validaImagem()) {
+                    if ($banco->query($q)) {
                         echo msg_sucesso("Jogo cadastrado com sucesso!");
-
+                        if (!$capa == "") {
+                            $target_dir = "fotos/";
+                            $target_file = $target_dir . basename($_FILES["capa"]["name"]);
+                            if (move_uploaded_file($_FILES["capa"]["tmp_name"], $target_file)) {
+                                echo msg_sucesso("Imagem de capa salva com sucesso!");
+                            } else {
+                                echo msg_erro("Erro ao salvar a imagem de capa!");
+                            }
+                        }
                     } else {
-                        echo "Sorry, there was an error uploading your file.";
+                        echo msg_erro('Não foi possível cadastrar o jogo!');
                     }
-                    
+                }else{
+                    echo msg_erro('Imagem inválida!');
                 }
+
             }
         }
         echo voltar();
